ab00fa0a907aeae6f53093cdb1e9556aa0d1d3cf
Renata Pigatto <renatapigatto@gmail.com>
2022-11-21 11:31:23 -0300

    perfil + artigos + formulario

---
+++ src/js/geral/index.js
@@ -0,0 +1,10 @@
+// mostrar menu perfil
+
+let menuPerfil = document.getElementById("menu-perfil");
+
+function mostrarMenu(){
+    menuPerfil.classList.toggle("janela-aberta");
+}
+
+//ferramenta de busca
+
